13981a426038097d5fe4a449cd7e9bf36b9d3510
jprafael <jprafael@student.dei.uc.pt>
2011-05-26 03:36:34 +0100

    camera start

--- project/src/Photon.cpp
+++ project/src/Photon.cpp
@@ -17,6 +17,12 @@ Photon::Photon(Ray& ray, double wavelength)
 /* nFrom refers to the light speed in the medium from where the photon comes */
 bool Photon::bounce(Scene& scene, unsigned int bouncesLeft, Photon& photon, double nFrom)
 {
+	if (bouncesLeft == 0)
+	{
+		photon = *this;
+		return true;
+	}
+
 	Intersection intersect;
 	Vector normal;
 
@@ -40,7 +46,7 @@ bool Photon::bounce(Scene& scene, unsigned int bouncesLeft, Photon& photon, doub
 	*/
 
 	 /* reflectance depends on the angle between photon's ray and the primitive's normal on the intersection point  */
-	double refl = intersect.prim->mat.reflectance(intersect.direction.angle(intersect.prim->normal(intersect.point)),nFrom);
+	//double refl = intersect.prim->mat.reflectance(intersect.direction.angle(intersect.prim->normal(intersect.point)),nFrom);
 		
 	/* continue -.-" */
 	photon.ray.direction = intersect.direction;
@@ -49,13 +55,12 @@ bool Photon::bounce(Scene& scene, unsigned int bouncesLeft, Photon& photon, doub
 	/* continue -.-" */
 	if (true) /* reflection: http://en.wikipedia.org/wiki/Reflection_%28mathematics%29 */
 	{
-		normal = intersect.prim.normal(intersect.point);
-		photon.ray.direction -= 2*photon.ray.direction.dot(normal)*normal;
-	}
-	
-	if (bouncesLeft > 0)
-		return photon.bounce(scene, bouncesLeft-1, photon, nFrom);
+		normal = intersect.prim->normal(intersect.point);
+		photon.ray.direction = photon.ray.direction - normal*2*photon.ray.direction.dot(normal);
 
+		return photon.bounce(scene, bouncesLeft-1, photon, nFrom);
+	} 
+	
 	return true;
 }
 
--- project/src/Ray.cpp
+++ project/src/Ray.cpp
@@ -1,4 +1,5 @@
 #include "Ray.hpp"
+#include "Scene.hpp"
 
 Ray::Ray()
 {
@@ -7,4 +8,21 @@ Ray::Ray()
 Ray::Ray(Point origin, Vector direction)
 	: origin(origin), direction(direction)
 {
+}
+
+Color Ray::getColor(const Scene& scene, int maxdepth) const
+{
+	Color c;
+	Intersection inter;
+
+	if (maxdepth == 0)
+		return Color(0,0,0);
+
+	if (!scene.intersect(*this, inter))
+		return Color(0,1,0); /* TODO: foreground color */
+
+	/* get color here */
+	
+	/* material color */
+	return inter.prim->mat.color;	
 }
