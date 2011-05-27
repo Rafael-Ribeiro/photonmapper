f0c611d7d1b05320079a442e35c870af4a108431
jprafael <jprafael@student.dei.uc.pt>
2011-05-27 23:49:55 +0100

    camera refactoring; bugged

--- project/src/Scene.cpp
+++ project/src/Scene.cpp
@@ -48,6 +48,7 @@ bool Scene::intersect(const Ray& ray, Intersection& intersect) const
 				intersect.prim = *it;
 				intersect.direction = ray.direction;
 
+				cerr << "DIR: " << intersect.direction.norm() << endl;
 				sqrd_dist = temp_dist;
 			}
 		}
